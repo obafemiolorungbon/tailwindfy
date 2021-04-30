f0dc047c142ecd883e799947e3382db8dc9883f7
Obafemi Joseph <obafedance@gmail.com>
2021-04-30 23:29:13 +0100

    Fix: fixed filepath from absolute to process.cwd

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src,dest} = require("gulp");
 const inject = require("gulp-inject-string");
 const fs = require("fs");
+const path = require("path");
 
 const getValues = ()=>{
   try{
@@ -18,12 +19,18 @@ const getValues = ()=>{
 
 const {filename,destination} = getValues()
 
-console.log(filename,destination);
+console.log(path.join(__dirname + filename),path.join(__dirname + destination));
 const createConfig =()=>{
         return src("./tailwind.config.cjs")
-          .pipe(inject.after("content:[",`"${filename}"`))
-          .pipe(dest(`${destination}`));
+          .pipe(inject.after("content:[",`"${path.join(__dirname + filename)}"`))
+          .pipe(dest(`${path.join(__dirname + destination)}`));
 }
+
+// const editConfig = ()=>{
+//   return src("./package.json")
+//     .pipe(inject.after("the file url for dumping files"))
+//     .pipe(dest("file to drop the file"))
+// }
 exports.compile = createConfig;
 
 
